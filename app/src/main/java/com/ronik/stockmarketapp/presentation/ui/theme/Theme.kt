355b7043abdafb50beb6dbd65c3ee33c84bc2943
Ronik.Limbani <ronik.limbani@p5infosoft.com>
2025-02-17 12:59:41 +0530

    Small Change

--- app/src/main/java/com/ronik/stockmarketapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/ronik/stockmarketapp/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.ronik.stockmarketapp.ui.theme
+package com.ronik.stockmarketapp.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,13 +8,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+
+
+private val DarkColorPalette = darkColorScheme(
+    primary = Color.Green,
+    background = DarkBlue,
+    onPrimary = Color.DarkGray,
+    onBackground = TextWhite
+)
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
+
+
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
--- app/src/main/java/com/ronik/stockmarketapp/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/ronik/stockmarketapp/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.ronik.stockmarketapp.ui.theme
+package com.ronik.stockmarketapp.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
